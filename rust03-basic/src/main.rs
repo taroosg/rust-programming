b6679375cd06affde9174dbadd02bb5dac8556c0
taroosg <root@taroosg.dev>
2021-03-02 16:37:59 +0900

    add lifetime

--- rust03-basic/src/main.rs
+++ rust03-basic/src/main.rs
@@ -102,4 +102,33 @@ fn main() {
   println!("{:?}", make_tupple(1, 2));
   println!("{:?}", make_tupple("hello", "world"));
   println!("{:?}", make_tupple(vec![1, 2, 3], vec![4, 5, 6]));
+
+  // 所有権と借用
+  struct Color {
+    r: i32,
+    g: i32,
+    b: i32,
+  }
+  let a = Color {
+    r: 255,
+    g: 255,
+    b: 255,
+  };
+  // let b = a;  // NG：こうすると所有権が移るのでaは値を持たなくなる
+  let b = &a; // OK：参照するだけなのでab両方使える
+  println!("{} {} {}", b.r, b.g, b.b);
+  println!("{} {} {}", a.r, a.g, a.b);
+
+  // ライフタイム
+  // xスタート
+  let mut x = 5;
+  // yスタート
+  let y = &x;
+  // yエンド
+  // zスタート
+  let z = &mut x;
+  println!("{}", z);
+  // zエンド
+  println!("{}", x);
+  // xエンド
 }
