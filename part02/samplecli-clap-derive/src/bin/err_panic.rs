973cc5ac533f9df25fbb788118dbe029d844de79
taroosg <root@taroosg.dev>
2021-03-04 14:31:52 +0900

    update add original error

--- part02/samplecli-clap-derive/src/bin/err_panic.rs
+++ part02/samplecli-clap-derive/src/bin/err_panic.rs
@@ -1,22 +1,42 @@
-fn get_int_from_file() -> Result<i32, String> {
+use std::fmt;
+// 独自のエラー型
+enum MyError {
+  Io(std::io::Error),
+  Num(std::num::ParseIntError),
+}
+
+impl fmt::Display for MyError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
+      MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
+    }
+  }
+}
+
+fn get_int_from_file() -> Result<i32, MyError> {
   let path = "number.txt";
 
   // read_to_stringできればエラーで早期リターン
-  let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+  let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
   num_str
     .trim()
     .parse::<i32>()
     // parseできたら2倍する
     .map(|t| t * 2)
     // parseできなければエラーメッセージを返す
-    .map_err(|e| e.to_string())
+    .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
   match get_int_from_file() {
     // 2倍された値
-    Ok(num) => println!("{}", num),
+    Ok(x) => println!("{}", x),
     // エラーメッセージ
+    // Err(e) => match e {
+    //   MyError::Io(cause) => println!("I/O Error: {}", cause),
+    //   MyError::Num(cause) => println!("Parse Error: {}", cause),
+    // },
     Err(e) => println!("{}", e),
   }
   // println!("{}", get_int_from_file());
