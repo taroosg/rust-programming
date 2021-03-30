795298daafcdfb87c25f2c3f6c4b237147a498c1
taroosg <root@taroosg.dev>
2021-03-30 15:51:08 +0900

    update mandelbrot canvas draw by js

--- part02/mandelbrot/www/index.js
+++ part02/mandelbrot/www/index.js
@@ -16,6 +16,47 @@ console.log("start loading wasm");
 const mandelbrot = import('../pkg')
   .catch(console.error);
 
+const logic = {
+  getNDiverged: function (x0, y0, max_iter) {
+    let xn = 0.0;
+    let yn = 0.0;
+    for (let i = 0; i < max_iter; i++) {
+      let x_next = xn * xn - yn * yn + x0;
+      let y_next = 2.0 * xn * yn + y0;
+      xn = x_next;
+      yn = y_next;
+      if (xn * xn + yn * yn > 4.0) {
+        return i;
+      }
+    }
+    return max_iter;
+  },
+  generateMandelbrotSet: function (
+    canvas_w,
+    canvas_h,
+    x_min,
+    x_max,
+    y_min,
+    y_max,
+    max_iter,
+  ) {
+    let data = [];
+    for (let i = 0; i < canvas_h; i++) {
+      let y = y_min + (y_max - y_min) * i / canvas_h;
+      for (let j = 0; j < canvas_w; j++) {
+        let x = x_min + (x_max - x_min) * j / canvas_w;
+        let iter_index = this.getNDiverged(x, y, max_iter);
+        let v = iter_index % 8 * 32;
+        data.push(v);// R
+        data.push(v);// G
+        data.push(v);// B
+        data.push(255);// A
+      }
+    }
+    return data;
+  }
+}
+
 Promise.all([mandelbrot]).then(([
   { generate_mandelbrot_set, draw_mandelbrot_set }
 ]) => {
@@ -39,6 +80,35 @@ Promise.all([mandelbrot]).then(([
       const elapsed = generateEndtime - generateStartTime;
       console.log(`\tgenerate:wasm\tgenerate_elapsed:${elapsed}[ms]`);
       console.log(`\tdraw: js\tdraw_elapsed: ${drawEndtime - drawStartTime}[ms]`)
+    }
+    let jsResult = null;
+    {
+      const CANVAS_ID = "canvas_js";
+      let canvas = document.getElementById(CANVAS_ID);
+      let context = canvas.getContext("2d");
+      const canvasWidth = canvas.width;
+      const canvasHeight = canvas.height;
+      const generateStartTime = Date.now();
+      jsResult = logic.generateMandelbrotSet(canvasWidth, canvasHeight, X_MIN, X_MAX, Y_MIN, Y_MAX, MAX_ITER);
+      const generateEndtime = Date.now();
+      const elapsed = generateEndtime - generateStartTime;
+      console.log(`generate:js\tgenerate_elapsed:${elapsed}[ms]`);
+      const drawStartTime = Date.now();
+      draw(context, canvasWidth, canvasHeight, jsResult);
+      const drawEndtime = Date.now();
+
+      console.log(`draw: js\tdraw_elapsed: ${drawEndtime - drawStartTime}[ms]`)
+    }
+    {
+      let isSame = true;
+      for (let i = 0; i < wasmResult; i++) {
+        if (wasmResult[i] !== jsResult[i]) {
+          console.log(i, wasmResult[i], jsResult[i]);
+          isSame = false;
+          break;
+        }
+      }
+      console.log(`\n(wasmResult === jsResult):${isSame}`);
     }
   });
 });
